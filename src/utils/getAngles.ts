481aafa5a8d0f4c17c4764fd2b3a840e6e79a935
GuRaBang <87111950+GuRaBang@users.noreply.github.com>
2023-02-26 01:32:49 +0900

    REFACTOR: modify variable name

--- src/utils/getAngles.ts
+++ src/utils/getAngles.ts
@@ -1,13 +1,13 @@
 function getAngles(date: Date) {
-  const [hour, min, sec] = [
+  const [hour, minute, second] = [
     date.getHours(),
     date.getMinutes(),
     date.getSeconds(),
   ];
-  const hourAngle = hour * 30 + min * (1 / 2) + sec * (1 / 120);
-  const minAngle = min * 6 + sec * (1 / 10);
-  const secAngle = sec * 6;
-  return [hourAngle, minAngle, secAngle];
+  const hourAngle = hour * 30 + minute * (1 / 2) + second * (1 / 120);
+  const minuteAngle = minute * 6 + second * (1 / 10);
+  const secondAngle = second * 6;
+  return [hourAngle, minuteAngle, secondAngle];
 }
 
 export default getAngles;
