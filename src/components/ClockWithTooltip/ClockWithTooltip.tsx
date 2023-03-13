2543187d049d85422f42ebd9139bc3d0404967fa
GuRaBang <qkrdbsgk00@gmail.com>
2023-03-14 01:51:38 +0900

    REFACTOR: change Tooltip position using ref
    - ref로 Tooltip의 스타일 속성에 접근하여 위치 조정
    - mouseenter, mouseleave 이벤트로 변경

--- src/components/ClockWithTooltip/ClockWithTooltip.tsx
+++ src/components/ClockWithTooltip/ClockWithTooltip.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState, useCallback } from "react";
+import { MouseEvent, useRef, useState, useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import Clock from "../Clock/Clock";
 import Tooltip from "../Tooltip/Tooltip";
@@ -10,34 +10,42 @@ interface ClockWithTooltipProps {
 
 function ClockWithTooltip({ tooltipHeight = 70 }: ClockWithTooltipProps) {
   const [active, setActive] = useState(false);
-  const [positionX, setPositionX] = useState<number>();
-  const [positionY, setPositionY] = useState<number>();
-
-  const activeTooltip = useCallback(() => {
-    setActive(true);
-  }, []);
-  const inactiveTooltip = useCallback(() => {
-    setActive(false);
-  }, []);
-  const moveTooltip = useCallback((e: MouseEvent<HTMLDivElement>) => {
-    setPositionX(e.pageX);
-    setPositionY(e.pageY);
-  }, []);
-
   const { formatTimeString } = useRecoilValue(currentTimeDataState);
+  const tooltipRef = useRef<HTMLDivElement>();
+  const mousePosition = useRef<{ x: number; y: number }>();
+
+  const activeTooltip = (e: MouseEvent<HTMLDivElement>) => {
+    // 툴팁 활성화 시 위치 조정
+    mousePosition.current = { x: e.pageX, y: e.pageY - tooltipHeight ?? 0 };
+    setActive(true);
+  };
+
+  const inactiveTooltip = () => {
+    setActive(false);
+  };
+
+  const moveTooltip = (e: MouseEvent<HTMLDivElement>) => {
+    // 마우스 이동 시 툴팁 위치 조정
+    if (!tooltipRef.current) return;
+    tooltipRef.current.style.transform = `translate3d(${e.pageX}px, ${
+      e.pageY - tooltipHeight ?? 0
+    }px, 0)`;
+  };
 
   return (
     <>
       <Clock
-        onMouseOver={activeTooltip}
-        onMouseOut={inactiveTooltip}
+        onMouseEnter={activeTooltip}
+        onMouseLeave={inactiveTooltip}
         onMouseMove={moveTooltip}
       />
       {active && (
         <Tooltip
+          style={{
+            transform: `translate3d(${mousePosition.current?.x}px, ${mousePosition.current?.y}px, 0)`,
+          }}
+          ref={tooltipRef}
           height={tooltipHeight}
-          positionX={positionX}
-          positionY={(positionY as number) - tooltipHeight}
         >
           {formatTimeString}
         </Tooltip>
